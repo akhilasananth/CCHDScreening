73c6e9d677d548f50d6dd907912337384179f749
uk1288 <ukeme@Ukemeobongs-MacBook-Air.local>
2017-04-29 19:54:08 -0400

    testRepeat pages fixed

--- frontend/src/js/index.controller.js
+++ frontend/src/js/index.controller.js
@@ -1,28 +1,30 @@
-var app = angular.module('ccdhApp',['ui.router']);
-        //app config
-        app.config(function($stateProvider, $urlRouterProvider){
-            $urlRouterProvider.otherwise('/home');
-            $stateProvider
-                        .state('/home',{
-                            url: '/home',
-                            templateUrl: '../views/homePage/home.html',
-                            controller: 'indexCtrl'
-                        })
-                        .state('/babyInformation',{
-                            url:'/babyInformation',
-                            templateUrl: '../views/babyInformation/babyInformation.html',
-                            controller:'indexCtrl'
-                        })
-                        .state('/hospitalInformation', {
-                            url:'/hospitalInformation',
-                            templateUrl:'../views/hospitalInfo/hospitalInfo.html',
-                            controller:'indexCtrl'
-                        })
-                        
-                        
-            
-        });
-        app.controller('indexCtrl', function($scope){
+angular.module('ccdhApp',['ui.router', /* 'moment-picker' */]);
 
-        })
+angular.module('ccdhApp').config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider
+                .state('/home',{
+                    url: '/home',
+                    templateUrl: '../views/homePage/home.html',
+                    controller: 'indexCtrl'
+                })
+                .state('/babyInformation',{
+                    url:'/babyInformation',
+                    templateUrl: '../views/babyInformation/babyInformation.html',
+                    controller:'indexCtrl'
+                })
+                .state('/hospitalInformation', {
+                    url:'/hospitalInformation',
+                    templateUrl:'../views/hospitalInfo/hospitalInfo.html',
+                    controller:'indexCtrl'
+                })
+                .state('/testRepeatScreen', {
+                    url:'/testRepeat',
+                    templateUrl:'../views/TestRepeatScreen/testRepeatScreen.html',
+                    controller:'indexCtrl'
+                })
+});
+angular
+    .module('ccdhApp')
+    .controller('indexCtrl', function($scope){ })
        
