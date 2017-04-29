f4ac07e5f8b10719a8de733c668dd48c74fb91ab
Ehren Lillies <ehren@outlook.com>
2017-04-29 16:33:34 -0400

    initial

---
+++ NetAlertsWeb/NetAlertsApi/Models/BTaskHour.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetAlertsWeb.Models
+{
+    public class BTaskHour
+    {
+    }
+}
